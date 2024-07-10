ad5dd41cf019b6681f2aa768962564a9f4005746
YurySubbota <plus375291421728@gmail.com>
2024-07-10 21:59:04 +0300

    exclude reserved equip from mainpage

--- djequipment/equipmentrental/views.py
+++ djequipment/equipmentrental/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from equipmentrental.models import Equipment, Photo
+from equipmentrental.cart import is_reserved
 
 
 def index(request):
     equipment = Equipment.objects.filter(status='ready').order_by('-id')
+    reserved = Equipment.objects.none()
+    for equip in equipment:
+        if is_reserved(equip.id):
+            reserv = Equipment.objects.filter(id=equip.id)
+            reserved = reserved.union(reserv)
+    equipment = equipment.difference(reserved)
     for equip in equipment:
         equip.photo = Photo.objects.filter(equipment=equip).first()
 
